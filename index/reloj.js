fec03545d4ff8d85377187e1a98fb0a1fc004c8e
SolYX1349 <saul77023@gmail.com>
2023-03-24 00:04:19 -0600

    Se agrego la funcionalidad de mostrar el dia mes y año

--- index/reloj.js
+++ index/reloj.js
@@ -1,12 +1,47 @@
-function reloj() {
+let diaSemanaArray = [
+  "Domingo",
+  "Lunes",
+  "Martes",
+  "Miercoles",
+  "Jueves",
+  "Viernes",
+];
+let monthArray = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+function tiempo() {
   setInterval(obtenerDatos, 1000);
 }
-
 function obtenerDatos() {
   let fecha = new Date();
-  let segundos = addZero(fecha.getSeconds());
-  let minutos = addZero(fecha.getMinutes());
-  let horas = fomatHoras(fecha.getHours());
+  asignarFecha(
+    diaSemanaArray[fecha.getDay()],
+    monthArray[fecha.getMonth()],
+    fecha.getFullYear()
+  );
+  asignarHora(
+    fomatHoras(fecha.getHours()),
+    addZero(fecha.getMinutes()),
+    addZero(fecha.getSeconds())
+  );
+}
+function asignarFecha(day, month, year) {
+  document.getElementById("dia").innerHTML = day;
+  document.getElementById("mes").innerHTML = month;
+  document.getElementById("year").innerHTML = year;
+}
+function asignarHora(horas, minutos, segundos) {
   document.getElementById("hora-dia").innerHTML = horas;
   document.getElementById("minuto-dia").innerHTML = minutos;
   document.getElementById("segundo-dia").innerHTML = segundos;
@@ -17,12 +52,12 @@ function addZero(argument) {
   }
   return argument;
 }
-function fomatHoras(argument){
-    if(argument >= 13 && argument <= 24){
-        argument = argument - 12;
-    }
-    if(argument >= 0 && argument <= 9){
-        argument = "0" + argument;
-    }
-    return argument;
+function fomatHoras(argument) {
+  if (argument >= 13 && argument <= 24) {
+    argument = argument - 12;
+  }
+  if (argument >= 0 && argument <= 9) {
+    argument = "0" + argument;
+  }
+  return argument;
 }
